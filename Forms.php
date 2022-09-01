7b5a82cecf5c0434db824461730dfaaec40cfc0f
Cassandra Minnaar <cassandra.minnaar@gmail.com>
2022-09-01 19:14:42 +0200

    I think i just used php?

--- Forms.php
+++ Forms.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World!";
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $psw = test_input($_POST["psw"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
 
 </body>
